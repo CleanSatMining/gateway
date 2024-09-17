1fa7ad765336650e0d093efec19ce327930f7e6d
Cyrille <cdurbal@cleansatmining.com>
2024-09-17 12:01:04 +0200

    fix: openapi

--- src/app/pages/api/graphql/index.ts
+++ src/app/pages/api/graphql/index.ts
@@ -1,3 +1,10 @@
-import { createBuiltMeshHTTPHandler } from "./../../../../../.mesh";
+//import { createBuiltMeshHTTPHandler } from "./../../../../../.mesh";
+//export default createBuiltMeshHTTPHandler();
 
-export default createBuiltMeshHTTPHandler();
+import { NextApiRequest, NextApiResponse } from "next";
+//import { createBuiltMeshHTTPHandler } from '@graphql-mesh/runtime';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("API GRAPHQL", req, res);
+};
---
+++ src/pages/api/graphql/index.ts
@@ -0,0 +1,10 @@
+import { createBuiltMeshHTTPHandler } from "./../../../../.mesh";
+export default createBuiltMeshHTTPHandler();
+
+//import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+//export default async (req: NextApiRequest, res: NextApiResponse) => {
+//  console.log("API GRAPHQL", req.body, res.status);
+//  return res.status(200).json({ message: "API GRAPHQL" });
+//};
